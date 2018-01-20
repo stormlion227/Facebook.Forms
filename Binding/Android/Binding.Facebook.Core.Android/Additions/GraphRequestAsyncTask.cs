ff1fc1a214a45d704afbb1e8b223ffa27f465168
Marko Rothstein <rothstein.marko@yandex.com>
2018-01-20 06:34:38 +0100

    Added Binding.Facebook.Core.Android and Binding.Facebook.Common.Android

---
+++ Binding/Android/Binding.Facebook.Core.Android/Additions/GraphRequestAsyncTask.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Java.Lang;
+
+namespace Com.Facebook
+{
+    public partial class GraphRequestAsyncTask : AsyncTask
+    {
+        protected override Java.Lang.Object DoInBackground(params Java.Lang.Object[] @params)
+        {
+            return (Java.Lang.Object)DoInBackground_Invoke((Java.Lang.Void[])@params);
+        }
+    }
+}
---
+++ Binding/Android/Binding.Facebook.Core.Android/Additions/GraphRequestBatch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Java.Lang;
+
+namespace Com.Facebook
+{
+    public partial class GraphRequestBatch : global::Java.Util.AbstractList
+    {
+        public override Java.Lang.Object Get(int index)
+        {
+            return (Java.Lang.Object)Get_Invoke(index);
+        }
+    }
+}
